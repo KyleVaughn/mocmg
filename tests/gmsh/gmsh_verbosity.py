7026c0574cb43404ec568483a0cb6522578f2f66
kyle <kcvaughn@umich.edu>
2020-06-12 11:06:32 -0400

    gmsh tests

--- tests/gmsh/gmsh_verbosity.py
+++ tests/gmsh/gmsh_verbosity.py
@@ -1,7 +1,8 @@
 import gmsh
+import sys
 
-def test_gmshVerbosity99():
-    gmshVerbosity = 99
+def test_gmshVerbosity(N):
+    gmshVerbosity = N
     gmsh.initialize()
     gmsh.option.setNumber("General.Terminal", 1)
     gmsh.option.setNumber("General.Verbosity", gmshVerbosity)
@@ -9,6 +10,9 @@ def test_gmshVerbosity99():
     gmsh.model.occ.synchronize()
     gmsh.model.mesh.setSize(gmsh.model.getEntities(0), 1)
     gmsh.model.mesh.generate(2)
+    gmsh.model.occ.addDisk(0,0,0,1,1,1)
     gmsh.finalize()
 
-test_gmshVerbosity99()
+
+arg = int(sys.argv[1])
+test_gmshVerbosity(arg)
---
+++ tests/test_gmsh.py
@@ -0,0 +1,47 @@
+import gmsh
+import subprocess
+from unittest import TestCase
+
+def run(cmd):
+    proc = subprocess.Popen(cmd,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+    )
+    stdout, stderr = proc.communicate()
+    return stdout, stderr
+
+# Need to find inexpensive example where gmsh gives a warning. When found, use instead of current
+class test_gmsh(TestCase):
+
+    # nosetests makes capturing output of gmsh difficult, so using subprocess is the solution for now.
+    def test_gmshVerbosityDebug(self):
+        out, err = run(['python','./tests/gmsh/gmsh_verbosity.py', '99'])
+        out, err = out.decode('ascii').splitlines(), err.decode('ascii').splitlines()
+        self.assertIn('Error   : OpenCASCADE surface with tag 1 already exists', err)
+        self.assertIn('Info    : Meshing 2D...', out)
+        self.assertIn('Debug   : Syncing OCC_Internals with GModel', out)
+
+    def test_gmshVerbosityInfo(self):
+        out, err = run(['python','./tests/gmsh/gmsh_verbosity.py', '5'])
+        out, err = out.decode('ascii').splitlines(), err.decode('ascii').splitlines()
+        self.assertIn('Error   : OpenCASCADE surface with tag 1 already exists', err)
+        self.assertIn('Info    : Meshing 2D...', out)
+        self.assertNotIn('Debug   : Syncing OCC_Internals with GModel', out)
+
+    def test_gmshVerbosityWarning(self):
+        out, err = run(['python','./tests/gmsh/gmsh_verbosity.py', '2'])
+        out, err = out.decode('ascii').splitlines(), err.decode('ascii').splitlines()
+        self.assertIn('Error   : OpenCASCADE surface with tag 1 already exists', err)
+        self.assertFalse(out)
+
+    def test_gmshVerbosityError(self):
+        out, err = run(['python','./tests/gmsh/gmsh_verbosity.py', '1'])
+        out, err = out.decode('ascii').splitlines(), err.decode('ascii').splitlines()
+        self.assertIn('Error   : OpenCASCADE surface with tag 1 already exists', err)
+        self.assertFalse(out)
+
+    def test_gmshVerbositySilent(self):
+        out, err = run(['python','./tests/gmsh/gmsh_verbosity.py', '0'])
+        out, err = out.decode('ascii').splitlines(), err.decode('ascii').splitlines()
+        self.assertIn('Error   : OpenCASCADE surface with tag 1 already exists', err)
+        self.assertFalse(err)
