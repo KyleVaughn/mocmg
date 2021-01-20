c42214cf81bd0b363a2929f506608f965124b7d2
kyle <kcvaughn@umich.edu>
2021-01-20 16:19:27 -0500

    Still working on rect grid. need recursive label. commented out to avoid complexity check

--- mocmg/gmsh_utils.py
+++ mocmg/gmsh_utils.py
@@ -1,6 +1,8 @@
 """A collection of functions to automate common tasks within gmsh."""
 import logging
 
+# from collections.abc import Iterable
+
 # import gmsh
 # import numpy as np
 
@@ -15,74 +17,153 @@ module_log = logging.getLogger(__name__)
 #    return 1
 
 
-def rectangular_grid(bb, x=None, y=None, nx=None, ny=None):
-    """Fill me in."""
-    module_log.info("Generating rectangular grid")
-    x_min, y_min, z_min = bb[0:3]
-    x_max, y_max, z_max = bb[3:6]
-    #    dx = x_max - x_min
-    #    dy = y_max - y_min
-    dz = z_max - z_min
-    #    z = z_min
-
-    # Error checking
-    # Number of args
-    arg_ctr = 0
-    arg_len = []
-    for arg in [x, y, nx, ny]:
-        if arg is not None:
-            arg_ctr = arg_ctr + 1
-            module_log.require(isinstance(arg, list), "Arguments should be list type.")
-            arg_len.append(len(arg))
-    module_log.require(
-        arg_ctr == 2,
-        "Incorrect number of arguments given. Provide one of (x or nx) and one of (ny or y).",
-    )
-    # Make sure that args are equal in levels.
-    # Should only be 2 args now after last check
-    module_log.require(
-        arg_len[0] == arg_len[1],
-        f"Length of arguments differ ({arg_len[0]} and {arg_len[1]})."
-        + " They should have the same number of levels.",
-    )
-    # Model thickness for 2D
-    module_log.require(
-        abs(dz) <= 1e-6,
-        f"Bounding box thickness is greater than 1e-6 ({dz:.6f})."
-        + " Bounding box expected in 2D x-y plane.",
-    )
-    # If using nx, ny, ensure that the members are empty or int
-    if nx is not None:
-        module_log.require(
-            not nx or all(isinstance(i, int) for i in nx),
-            "nx must be empty or contain integer elements only",
-        )
-
-    if ny is not None:
-        module_log.require(
-            not ny or all(isinstance(i, int) for i in ny),
-            "ny must be empty or contain integer elements only",
-        )
-
-    # Compute a few necessary quantities
-
-
-#    max_grid_digits = len(str(np.prod(nx))) # Digits on grid label
-#    width = dx/np.prod(nx)
-#    height = dy/np.prod(ny)
-
-#    # Create the smallest level of rectangles
-#    grid_tags = []
-#    y = y_min
-#    for j in range(np.prod(ny)):
-#        x = x_min
-#        for i in range(np.prod(nx)):
-#            tag = gmsh.model.occ.addRectangle(x, y, z, width, height)
-#            print(tag)
-#            x = x + width
-#        y = y + height
+# def rectangular_grid(bb, x=None, y=None, nx=None, ny=None):
+#    """Fill me in."""
+#    module_log.info("Generating rectangular grid")
+#    x_min, y_min, z_min = bb[0:3]
+#    x_max, y_max, z_max = bb[3:6]
+#    dx = x_max - x_min
+#    dy = y_max - y_min
+#    dz = z_max - z_min
+#    zz = z_min
 #
+#    # Error checking
+#    # dx, dy, dz should all be positive
+#    for d in [dx, dy, dz]:
+#        module_log.require(d >= 0, "Invalid bounding box.")
+#    # Number of args
+#    arg_ctr = 0
+#    arg_len = []
+#    for arg in [x, y, nx, ny]:
+#        if arg is not None:
+#            arg_ctr = arg_ctr + 1
+#            module_log.require(isinstance(arg, Iterable), "Arguments should be iterable.")
+#            arg_len.append(len(arg))
+#    module_log.require(
+#        arg_ctr == 2,
+#        "Incorrect number of arguments given. Provide one of (x or nx) and one of (ny or y).",
+#    )
+#    # Make sure that args are equal in levels.
+#    # Should only be 2 args now after last check
+#    module_log.require(
+#        arg_len[0] == arg_len[1],
+#        f"Length of arguments differ ({arg_len[0]} and {arg_len[1]})."
+#        + " They should have the same number of levels.",
+#    )
+#    nlevels = arg_len[0]
+#    # Model thickness for 2D
+#    module_log.require(
+#        abs(dz) <= 1e-6,
+#        f"Bounding box thickness is greater than 1e-6 ({dz:.6f})."
+#        + " Bounding box expected in 2D x-y plane.",
+#    )
+#    # If using nx, ny, ensure that the members are empty or int
+#    if nx is not None:
+#        module_log.require(
+#            not nx or all(isinstance(i, int) for i in nx),
+#            "nx must be empty or contain integer elements only.",
+#        )
+#    if ny is not None:
+#        module_log.require(
+#            not ny or all(isinstance(i, int) for i in ny),
+#            "ny must be empty or contain integer elements only.",
+#        )
+#    # If x, y, ensure that elements are iterable
+#    if x is not None:
+#        module_log.require(
+#            all(isinstance(i, Iterable) for i in x),
+#            "x must have iterable elements.",
+#        )
+#    if y is not None:
+#        module_log.require(
+#            all(isinstance(i, Iterable) for i in y),
+#            "y must have iterable elements.",
+#        )
+#
+#    # Convert nx, ny notation to x, y format
+#    # Include bb limits
+#    if x is not None:
+#        x[0] = list(x[0])
+#        x[0] = sorted(set(x[0] + [x_min, x_max]))
+#        for lvl in range(1, nlevels):
+#            x[lvl] = sorted(set(x[lvl - 1] + x[lvl]))
+#    if y is not None:
+#        y[0] = list(y[0])
+#        y[0] = sorted(set(y[0] + [y_min, y_max]))
+#        for lvl in range(1, nlevels):
+#            y[lvl] = sorted(set(y[lvl - 1] + y[lvl]))
+#    if nx is not None:
+#        # Get first level intervals
+#        x = []
+#        div = nx[0]
+#        if div == 0:
+#            div = 1
+#        x.append(list(np.linspace(x_min, x_max, div + 1, endpoint=True)))
+#        # If there is more than one level, divide intervals further
+#        for lvl in range(1, nlevels):
+#            x.append([])
+#            for i in range(len(x[lvl - 1]) - 1):
+#                x[lvl].append(
+#                    np.linspace(x[lvl - 1][i], x[lvl - 1][i + 1], nx[lvl] + 1, endpoint=True)
+#                )
+#            # Get rid of duplicates
+#            x[lvl] = sorted(set(np.append(x[lvl][0], x[lvl][1:])))
+#    if ny is not None:
+#        # Get first level intervals
+#        y = []
+#        div = ny[0]
+#        if div == 0:
+#            div = 1
+#        y.append(list(np.linspace(y_min, y_max, div + 1, endpoint=True)))
+#        # If there is more than one level, divide intervals further
+#        for lvl in range(1, nlevels):
+#            y.append([])
+#            for i in range(len(y[lvl - 1]) - 1):
+#                y[lvl].append(
+#                    np.linspace(y[lvl - 1][i], y[lvl - 1][i + 1], ny[lvl] + 1, endpoint=True)
+#                )
+#            # Get rid of duplicates
+#            y[lvl] = sorted(set(np.append(y[lvl][0], y[lvl][1:])))
+#
+#    # Create smallest rectangles
+#    # Ensure elements are in the bb
+#    module_log.require(
+#        all(x_min <= xx and xx <= x_max for xx in x[-1]),
+#        "Divisions must be within the bounding box.",
+#    )
+#    module_log.require(
+#        all(y_min <= yy and yy <= y_max for yy in y[-1]),
+#        "Divisions must be within the bounding box.",
+#    )
+#    grid_tags_coords = []
+#    for y_ind, yy in enumerate(y[-1][:-1]):
+#        for x_ind, xx in enumerate(x[-1][:-1]):
+#            tag = gmsh.model.occ.addRectangle(
+#                xx, yy, zz, x[-1][x_ind + 1] - xx, y[-1][y_ind + 1] - yy
+#            )
+#            grid_tags_coords.append([tag, xx, yy])
 #    gmsh.model.occ.synchronize()
 
+# Label the rectangles with the appropriate grid level and location
+# level_#_(i,j) like so:
+# y
+# ^
+# |------------
+# |(1,2)|(2,2)|
+# |-----|-----|
+# |(1,1)|(2,1)|
+# --------------> x
 
-#    objects = _make_rectangular_grid_level(bb, nx, ny, 0)
+
+#    total_rect = (len(x[-1]) - 1) * (len(x[-1]) - 1)
+
+
+#    max_grid_digits = len(str(total_rect))  # Digits on grid label
+#    level_tags = []
+#    for _i in range(nlevels):
+#        level_tags.append([])
+#    # Lowest level
+#    for t in grid_tags:
+#        level_tags[nlevels - 1].append(t)
+#    # All other levels
+#    print(level_tags)
--- tests/test_gmsh_utils.py
+++ tests/test_gmsh_utils.py
@@ -17,12 +17,15 @@ reference_out = [
     "INFO      : mocmg.gmsh_utils - Generating rectangular grid",
 ]
 
+# Expected error for a bad bounding box that produces negative dx, dy, dz
+bad_bb = [
+    "ERROR     : mocmg.gmsh_utils - Invalid bounding box.",
+]
 
 # Expected error for argument of incorrect type
 bad_type = [
-    "ERROR     : mocmg.gmsh_utils - Arguments should be list type.",
+    "ERROR     : mocmg.gmsh_utils - Arguments should be iterable.",
 ]
-
 
 # Expected error for mismatch of x and y size
 len_mismatch = [
@@ -42,15 +45,51 @@ thick_dz = [
     + " (10.000000). Bounding box expected in 2D x-y plane.",
 ]
 
+# Expected error for non-int nx elements
+nx_type = ["ERROR     : mocmg.gmsh_utils - nx must be empty or contain integer elements only."]
+
+# Expected error for non-int nx elements
+ny_type = ["ERROR     : mocmg.gmsh_utils - ny must be empty or contain integer elements only."]
+
+# Expected error for non-list x elements
+x_nonlist_type = ["ERROR     : mocmg.gmsh_utils - x must have iterable elements."]
+
+# Expected error for non-list y elements
+y_nonlist_type = ["ERROR     : mocmg.gmsh_utils - y must have iterable elements."]
+
+# Expected error when divisions outside of bb
+out_of_bb = ["ERROR     : mocmg.gmsh_utils - Divisions must be within the bounding box."]
+
 
 class TestRectangularGrid(TestCase):
     """Test the gmsh_utils.rectangular_grid function."""
+
+    def test_bad_bounding_box(self):
+        """Test rect grid with non-list args."""
+        with pytest.raises(SystemExit):
+            with captured_output() as (out, err):
+                mocmg.initialize()
+                gmsh.initialize()
+                rectangular_grid([0, 0, 0, -1, -1, -1], x=[[0.5]], y=[[0.5]])
+                gmsh.clear()
+                gmsh.finalize()
+        out, err = out.getvalue().splitlines(), err.getvalue().splitlines()
+        out = [line.split(None, 1)[1] for line in out]
+        err = [line.split(None, 1)[1] for line in [err[0]]]  # strip times
+        self.assertEqual(out, reference_out)
+        self.assertEqual(err, bad_bb)
+        # check log file
+        f = open("mocmg.log", "r")
+        lines = f.readlines()
+        f.close()
+        lines = [line.split(None, 1)[1].rstrip("\n") for line in lines]
+        self.assertEqual(lines, reference_out + bad_bb)
 
     def test_bad_arg_type(self):
         """Test rect grid with non-list args."""
         with pytest.raises(SystemExit):
             with captured_output() as (out, err):
-                mocmg.initialize(exit_on_error=True)
+                mocmg.initialize()
                 gmsh.initialize()
                 rectangular_grid(bb_11, x=[[0.5]], y=[[0.5]], nx=4)
                 gmsh.clear()
@@ -71,7 +110,7 @@ class TestRectangularGrid(TestCase):
         """Test rect grid with too many args."""
         with pytest.raises(SystemExit):
             with captured_output() as (out, err):
-                mocmg.initialize(exit_on_error=True)
+                mocmg.initialize()
                 gmsh.initialize()
                 rectangular_grid(bb_11, x=[[0.5]], y=[[0.5]], nx=[4])
                 gmsh.clear()
@@ -92,7 +131,7 @@ class TestRectangularGrid(TestCase):
         """Test rect grid with too few args."""
         with pytest.raises(SystemExit):
             with captured_output() as (out, err):
-                mocmg.initialize(exit_on_error=True)
+                mocmg.initialize()
                 gmsh.initialize()
                 rectangular_grid(bb_11, x=[[0.5]])
                 gmsh.clear()
@@ -113,7 +152,7 @@ class TestRectangularGrid(TestCase):
         """Test rect grid with mismatched arg len."""
         with pytest.raises(SystemExit):
             with captured_output() as (out, err):
-                mocmg.initialize(exit_on_error=True)
+                mocmg.initialize()
                 gmsh.initialize()
                 rectangular_grid(bb_11, x=[[0.5], [0.25]], y=[[0.5]])
                 gmsh.clear()
@@ -134,7 +173,7 @@ class TestRectangularGrid(TestCase):
         """Test rect grid with large change in z direction."""
         with pytest.raises(SystemExit):
             with captured_output() as (out, err):
-                mocmg.initialize(exit_on_error=True)
+                mocmg.initialize()
                 gmsh.initialize()
                 rectangular_grid(bb_dz, x=[[0.5]], y=[[0.5]])
                 gmsh.clear()
@@ -150,3 +189,165 @@ class TestRectangularGrid(TestCase):
         f.close()
         lines = [line.split(None, 1)[1].rstrip("\n") for line in lines]
         self.assertEqual(lines, reference_out + thick_dz)
+
+    def test_nx_type(self):
+        """Test rect grid with non-integer type elements."""
+        with pytest.raises(SystemExit):
+            with captured_output() as (out, err):
+                mocmg.initialize()
+                gmsh.initialize()
+                rectangular_grid(bb_11, nx=[0.1], y=[[0.5]])
+                gmsh.clear()
+                gmsh.finalize()
+        out, err = out.getvalue().splitlines(), err.getvalue().splitlines()
+        out = [line.split(None, 1)[1] for line in out]
+        err = [line.split(None, 1)[1] for line in [err[0]]]  # strip times
+        self.assertEqual(out, reference_out)
+        self.assertEqual(err, nx_type)
+        # check log file
+        f = open("mocmg.log", "r")
+        lines = f.readlines()
+        f.close()
+        lines = [line.split(None, 1)[1].rstrip("\n") for line in lines]
+        self.assertEqual(lines, reference_out + nx_type)
+
+    def test_ny_type(self):
+        """Test rect grid with non-integer type elements."""
+        with pytest.raises(SystemExit):
+            with captured_output() as (out, err):
+                mocmg.initialize()
+                gmsh.initialize()
+                rectangular_grid(bb_11, x=[[0.1]], ny=[0.5])
+                gmsh.clear()
+                gmsh.finalize()
+        out, err = out.getvalue().splitlines(), err.getvalue().splitlines()
+        out = [line.split(None, 1)[1] for line in out]
+        err = [line.split(None, 1)[1] for line in [err[0]]]  # strip times
+        self.assertEqual(out, reference_out)
+        self.assertEqual(err, ny_type)
+        # check log file
+        f = open("mocmg.log", "r")
+        lines = f.readlines()
+        f.close()
+        lines = [line.split(None, 1)[1].rstrip("\n") for line in lines]
+        self.assertEqual(lines, reference_out + ny_type)
+
+    def test_x_nonlist_type(self):
+        """Test rect grid with non-integer type elements."""
+        with pytest.raises(SystemExit):
+            with captured_output() as (out, err):
+                mocmg.initialize()
+                gmsh.initialize()
+                rectangular_grid(bb_11, x=[1], ny=[1])
+                gmsh.clear()
+                gmsh.finalize()
+        out, err = out.getvalue().splitlines(), err.getvalue().splitlines()
+        out = [line.split(None, 1)[1] for line in out]
+        err = [line.split(None, 1)[1] for line in [err[0]]]  # strip times
+        self.assertEqual(out, reference_out)
+        self.assertEqual(err, x_nonlist_type)
+        # check log file
+        f = open("mocmg.log", "r")
+        lines = f.readlines()
+        f.close()
+        lines = [line.split(None, 1)[1].rstrip("\n") for line in lines]
+        self.assertEqual(lines, reference_out + x_nonlist_type)
+
+    def test_y_nonlist_type(self):
+        """Test rect grid with non-integer type elements."""
+        with pytest.raises(SystemExit):
+            with captured_output() as (out, err):
+                mocmg.initialize()
+                gmsh.initialize()
+                rectangular_grid(bb_11, x=[[1]], y=[1.0])
+                gmsh.clear()
+                gmsh.finalize()
+        out, err = out.getvalue().splitlines(), err.getvalue().splitlines()
+        out = [line.split(None, 1)[1] for line in out]
+        err = [line.split(None, 1)[1] for line in [err[0]]]  # strip times
+        self.assertEqual(out, reference_out)
+        self.assertEqual(err, y_nonlist_type)
+        # check log file
+        f = open("mocmg.log", "r")
+        lines = f.readlines()
+        f.close()
+        lines = [line.split(None, 1)[1].rstrip("\n") for line in lines]
+        self.assertEqual(lines, reference_out + y_nonlist_type)
+
+    def test_x_out_of_bb(self):
+        """Test rect grid with an x-division outside the bb."""
+        with pytest.raises(SystemExit):
+            with captured_output() as (out, err):
+                mocmg.initialize()
+                gmsh.initialize()
+                rectangular_grid(bb_11, x=[[-1.0]], y=[[1.0]])
+                gmsh.clear()
+                gmsh.finalize()
+        out, err = out.getvalue().splitlines(), err.getvalue().splitlines()
+        out = [line.split(None, 1)[1] for line in out]
+        err = [line.split(None, 1)[1] for line in [err[0]]]  # strip times
+        self.assertEqual(out, reference_out)
+        self.assertEqual(err, out_of_bb)
+        # check log file
+        f = open("mocmg.log", "r")
+        lines = f.readlines()
+        f.close()
+        lines = [line.split(None, 1)[1].rstrip("\n") for line in lines]
+        self.assertEqual(lines, reference_out + out_of_bb)
+
+    def test_y_out_of_bb(self):
+        """Test rect grid with a y-division outside the bb."""
+        with pytest.raises(SystemExit):
+            with captured_output() as (out, err):
+                mocmg.initialize()
+                gmsh.initialize()
+                rectangular_grid(bb_11, x=[[1.0]], y=[[-1.0]])
+                gmsh.clear()
+                gmsh.finalize()
+        out, err = out.getvalue().splitlines(), err.getvalue().splitlines()
+        out = [line.split(None, 1)[1] for line in out]
+        err = [line.split(None, 1)[1] for line in [err[0]]]  # strip times
+        self.assertEqual(out, reference_out)
+        self.assertEqual(err, out_of_bb)
+        # check log file
+        f = open("mocmg.log", "r")
+        lines = f.readlines()
+        f.close()
+        lines = [line.split(None, 1)[1].rstrip("\n") for line in lines]
+        self.assertEqual(lines, reference_out + out_of_bb)
+
+    #    def test_nx_0_ny_0(self):
+    #        """Test nx=[0]."""
+    #        mocmg.initialize()
+    #        gmsh.initialize()
+    #        rectangular_grid(bb_11, nx=[0], ny=[0])
+    #        gmsh.clear()
+    #        gmsh.finalize()
+    #        self.assertEqual(1,2)
+    #
+    #    def test_x_0_y_0(self):
+    #        """Test x with empty set and divisions=0."""
+    #        mocmg.initialize()
+    #        gmsh.initialize()
+    #        rectangular_grid(bb_11, x=[[]], y=[[0]])
+    #        gmsh.clear()
+    #        gmsh.finalize()
+    #        self.assertEqual(1,2)
+
+    #    def test_n(self):
+    #        """Test nx=[0]."""
+    #        mocmg.initialize()
+    #        gmsh.initialize()
+    #        rectangular_grid(bb_11, nx=[2,2,2], ny=[2,2,2])
+    #        gmsh.clear()
+    #        gmsh.finalize()
+    #        self.assertEqual(1,2)
+
+    def test_n(self):
+        """Test nx=[0]."""
+        mocmg.initialize()
+        gmsh.initialize()
+        rectangular_grid(bb_11, x=[[0.5], [0.25, 0.75]], y=[[0.5], [0.25, 0.75]])
+        gmsh.clear()
+        gmsh.finalize()
+        self.assertEqual(1, 2)
