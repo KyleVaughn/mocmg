e796e4a9703dc2d8a26049b3207b9111f3b829ec
kyle <kcvaughn@umich.edu>
2021-02-22 17:53:42 -0500

    Added ability to overwrite a material in the fragmentation process.

--- mocmg/model/group_preserving_fragment.py
+++ mocmg/model/group_preserving_fragment.py
@@ -5,8 +5,12 @@ import gmsh
 
 module_log = logging.getLogger(__name__)
 
+# TODO: Update overwrite_material to allow for overwriting one material in all other materials
+# OR accepting a map of how to overwrite for entities with multiple materials. This would allow
+# For more complex fragmenting that will still keep groups.
 
-def group_preserving_fragment(object_dim_tags, tool_dim_tags):
+
+def group_preserving_fragment(object_dim_tags, tool_dim_tags, overwrite_material=None):
     """Fragment CAD entities with gmsh, but preserve physical groups.
 
     Compute the boolean fragments (general fuse) of the entities in object_dim_tags and
@@ -21,6 +25,10 @@ def group_preserving_fragment(object_dim_tags, tool_dim_tags):
 
         tool_dim_tags (list): A list of the dim tags of entities to fragment.
 
+        overwrite_material (str): Name of material to overwrite, if
+            an entity has multiple materials after fragmentation. Used primarily to prevent
+            entities from inheriting the moderator/grid material.
+
     Returns:
         list : A list of the resultant dim tags.
     """
@@ -34,7 +42,7 @@ def group_preserving_fragment(object_dim_tags, tool_dim_tags):
         old_physical_groups[name] = [(dim, ent) for ent in ents]
 
     # Fragment
-    module_log.info(f"Fragmenting {len(object_dim_tags + tool_dim_tags)} entities.")
+    module_log.info(f"Fragmenting {len(object_dim_tags + tool_dim_tags)} entities")
     out_dim_tags, out_dim_tags_map = gmsh.model.occ.fragment(object_dim_tags, tool_dim_tags)
 
     # Create dictionary of new physical groups using the parent child relationship
@@ -60,10 +68,18 @@ def group_preserving_fragment(object_dim_tags, tool_dim_tags):
                 # If it wasn't in the fragment, no changes necessary.
                 new_physical_groups[name].append(dim_tag)
 
+    # Sort the new physical groups to aid in debugging.
+    for name in new_physical_groups:
+        new_physical_groups[name].sort(key=lambda x: x[1])
+
     # Synchronize and remove old groups
     module_log.info("Synchronizing model")
     gmsh.model.occ.synchronize()
     gmsh.model.removePhysicalGroups()
+
+    # If overwriting materials
+    if overwrite_material is not None:
+        _overwrite_material(new_physical_groups, overwrite_material, names)
 
     # Create new physical groups
     for i, name in enumerate(names):
@@ -74,3 +90,27 @@ def group_preserving_fragment(object_dim_tags, tool_dim_tags):
         gmsh.model.setPhysicalName(dim, ptag, name)
 
     return out_dim_tags
+
+
+def _overwrite_material(new_physical_groups, overwrite_material, names):
+    module_log.require(
+        overwrite_material in names,
+        "Material to be overwritten is not in the physical groups of the entities being fragmented.",
+    )
+    # Collect all material entities
+    # Have to create deep copy here
+    overwrite_material_ents = list([*new_physical_groups[overwrite_material]])
+    other_material_ents = []
+    for name in names:
+        if name == overwrite_material:
+            continue
+        if "MATERIAL" in name.upper():
+            other_material_ents = other_material_ents + new_physical_groups[name]
+
+    # Remove entities which are both overwrite_material and another material from the set
+    # of entities composing overwrite_material
+    for ent in new_physical_groups[overwrite_material]:
+        if ent in other_material_ents:
+            overwrite_material_ents.remove(ent)
+    # Update the entities in the overwrite_material group
+    new_physical_groups[overwrite_material] = overwrite_material_ents
--- tests/test_group_preserving_fragment.py
+++ tests/test_group_preserving_fragment.py
@@ -2,15 +2,31 @@
 from unittest import TestCase
 
 import gmsh
+import pytest
 
 import mocmg
 
+from .testing_utils import captured_output
+
 # Honestly, just use gmsh.fltk.run() to debug.
+ref_out = [
+    "INFO      : mocmg.model.group_preserving_fragment - Fragmenting 2 entities",
+    "INFO      : mocmg.model.group_preserving_fragment - Synchronizing model",
+]
+bad_overwrite = [
+    "ERROR     : mocmg.model.group_preserving_fragment - Material to be overwritten"
+    + " is not in the physical groups of the entities being fragmented."
+]
 
 # 2 intersecting rectangles
 groups_2_rectangles = {
     "Group 1": [1, 2],
     "Group 2": [2, 3],
+    "All": [1, 2, 3],
+}
+groups_2_rectangles_w_materials = {
+    "Material_1": [1],
+    "Material_2": [2, 3],
     "All": [1, 2, 3],
 }
 centroids_2_rectangles = {
@@ -49,23 +65,29 @@ class TestGroupPreservingFragment(TestCase):
     """Test the group_preserving_fragment function."""
 
     def test_2_rectangles(self):
-        """Test the get_entities_for_physical_group_name for a 2 rectangle case."""
+        """Test a 2 rectangle case."""
         ref_groups = groups_2_rectangles
         ref_centroids = centroids_2_rectangles
         ref_areas = areas_2_rectangles
         # Setup
-        mocmg.initialize()
-        gmsh.initialize()
-        gmsh.model.occ.addRectangle(0.0, 0.0, 0.0, 2.0, 2.0)
-        gmsh.model.occ.addRectangle(1.0, 1.0, 0.0, 2.0, 2.0)
-        gmsh.model.occ.synchronize()
-        gmsh.model.addPhysicalGroup(2, [1])
-        gmsh.model.addPhysicalGroup(2, [2])
-        gmsh.model.addPhysicalGroup(2, [1, 2])
-        gmsh.model.setPhysicalName(2, 1, "Group 1")
-        gmsh.model.setPhysicalName(2, 2, "Group 2")
-        gmsh.model.setPhysicalName(2, 3, "All")
+        with captured_output() as (out, err):
+            mocmg.initialize()
+            gmsh.initialize()
+            gmsh.model.occ.addRectangle(0.0, 0.0, 0.0, 2.0, 2.0)
+            gmsh.model.occ.addRectangle(1.0, 1.0, 0.0, 2.0, 2.0)
+            gmsh.model.occ.synchronize()
+            gmsh.model.addPhysicalGroup(2, [1])
+            gmsh.model.addPhysicalGroup(2, [2])
+            gmsh.model.addPhysicalGroup(2, [1, 2])
+            gmsh.model.setPhysicalName(2, 1, "Group 1")
+            gmsh.model.setPhysicalName(2, 2, "Group 2")
+            gmsh.model.setPhysicalName(2, 3, "All")
         mocmg.model.group_preserving_fragment([(2, 1)], [(2, 2)])
+        out, err = out.getvalue().splitlines(), err.getvalue().splitlines()
+        out = [line.split(None, 1)[1] for line in out]
+        err = [line.split(None, 1)[1] for line in err]  # strip times
+        self.assertEqual(out, ref_out)
+        self.assertEqual(err, [])
         # Get info
         group_nums = gmsh.model.getPhysicalGroups()
         names = [gmsh.model.getPhysicalName(*grp) for grp in group_nums]
@@ -96,14 +118,20 @@ class TestGroupPreservingFragment(TestCase):
         ref_centroids = centroids_2_rectangles
         ref_areas = areas_2_rectangles
         # Setup
-        mocmg.initialize()
-        gmsh.initialize()
-        gmsh.model.occ.addRectangle(0.0, 0.0, 0.0, 2.0, 2.0)
-        gmsh.model.occ.addRectangle(1.0, 1.0, 0.0, 2.0, 2.0)
-        gmsh.model.occ.synchronize()
-        gmsh.model.addPhysicalGroup(2, [1])
-        gmsh.model.setPhysicalName(2, 1, "Group 1")
-        mocmg.model.group_preserving_fragment([(2, 1)], [(2, 2)])
+        with captured_output() as (out, err):
+            mocmg.initialize()
+            gmsh.initialize()
+            gmsh.model.occ.addRectangle(0.0, 0.0, 0.0, 2.0, 2.0)
+            gmsh.model.occ.addRectangle(1.0, 1.0, 0.0, 2.0, 2.0)
+            gmsh.model.occ.synchronize()
+            gmsh.model.addPhysicalGroup(2, [1])
+            gmsh.model.setPhysicalName(2, 1, "Group 1")
+            mocmg.model.group_preserving_fragment([(2, 1)], [(2, 2)])
+        out, err = out.getvalue().splitlines(), err.getvalue().splitlines()
+        out = [line.split(None, 1)[1] for line in out]
+        err = [line.split(None, 1)[1] for line in err]  # strip times
+        self.assertEqual(out, ref_out)
+        self.assertEqual(err, [])
         # Get info
         group_nums = gmsh.model.getPhysicalGroups()
         names = [gmsh.model.getPhysicalName(*grp) for grp in group_nums]
@@ -134,21 +162,27 @@ class TestGroupPreservingFragment(TestCase):
         ref_centroids = centroids_3_rectangles_1_not_in_frag
         ref_areas = areas_3_rectangles_1_not_in_frag
         # Setup
-        mocmg.initialize()
-        gmsh.initialize()
-        gmsh.model.occ.addRectangle(0.0, 0.0, 0.0, 2.0, 2.0)
-        gmsh.model.occ.addRectangle(1.0, 1.0, 0.0, 2.0, 2.0)
-        gmsh.model.occ.addRectangle(4.0, 4.0, 0.0, 2.0, 2.0)
-        gmsh.model.occ.synchronize()
-        gmsh.model.addPhysicalGroup(2, [1])
-        gmsh.model.addPhysicalGroup(2, [2])
-        gmsh.model.addPhysicalGroup(2, [3])
-        gmsh.model.addPhysicalGroup(2, [1, 2, 3])
-        gmsh.model.setPhysicalName(2, 1, "Group 1")
-        gmsh.model.setPhysicalName(2, 2, "Group 2")
-        gmsh.model.setPhysicalName(2, 3, "Group 3")
-        gmsh.model.setPhysicalName(2, 4, "All")
-        mocmg.model.group_preserving_fragment([(2, 1)], [(2, 2)])
+        with captured_output() as (out, err):
+            mocmg.initialize()
+            gmsh.initialize()
+            gmsh.model.occ.addRectangle(0.0, 0.0, 0.0, 2.0, 2.0)
+            gmsh.model.occ.addRectangle(1.0, 1.0, 0.0, 2.0, 2.0)
+            gmsh.model.occ.addRectangle(4.0, 4.0, 0.0, 2.0, 2.0)
+            gmsh.model.occ.synchronize()
+            gmsh.model.addPhysicalGroup(2, [1])
+            gmsh.model.addPhysicalGroup(2, [2])
+            gmsh.model.addPhysicalGroup(2, [3])
+            gmsh.model.addPhysicalGroup(2, [1, 2, 3])
+            gmsh.model.setPhysicalName(2, 1, "Group 1")
+            gmsh.model.setPhysicalName(2, 2, "Group 2")
+            gmsh.model.setPhysicalName(2, 3, "Group 3")
+            gmsh.model.setPhysicalName(2, 4, "All")
+            mocmg.model.group_preserving_fragment([(2, 1)], [(2, 2)])
+        out, err = out.getvalue().splitlines(), err.getvalue().splitlines()
+        out = [line.split(None, 1)[1] for line in out]
+        err = [line.split(None, 1)[1] for line in err]  # strip times
+        self.assertEqual(out, ref_out)
+        self.assertEqual(err, [])
         # Get info
         group_nums = gmsh.model.getPhysicalGroups()
         names = [gmsh.model.getPhysicalName(*grp) for grp in group_nums]
@@ -172,3 +206,79 @@ class TestGroupPreservingFragment(TestCase):
         # Clean up
         gmsh.clear()
         gmsh.finalize()
+
+    def test_2_rectangles_with_overwrite(self):
+        """Test a 2 rectangle case, overwriting a material."""
+        ref_groups = groups_2_rectangles_w_materials
+        ref_centroids = centroids_2_rectangles
+        ref_areas = areas_2_rectangles
+        # Setup
+        with captured_output() as (out, err):
+            mocmg.initialize()
+            gmsh.initialize()
+            gmsh.model.occ.addRectangle(0.0, 0.0, 0.0, 2.0, 2.0)
+            gmsh.model.occ.addRectangle(1.0, 1.0, 0.0, 2.0, 2.0)
+            gmsh.model.occ.synchronize()
+            gmsh.model.addPhysicalGroup(2, [1])
+            gmsh.model.addPhysicalGroup(2, [2])
+            gmsh.model.addPhysicalGroup(2, [1, 2])
+            gmsh.model.setPhysicalName(2, 1, "Material_1")
+            gmsh.model.setPhysicalName(2, 2, "Material_2")
+            gmsh.model.setPhysicalName(2, 3, "All")
+            mocmg.model.group_preserving_fragment(
+                [(2, 1)], [(2, 2)], overwrite_material="Material_1"
+            )
+        out, err = out.getvalue().splitlines(), err.getvalue().splitlines()
+        out = [line.split(None, 1)[1] for line in out]
+        err = [line.split(None, 1)[1] for line in err]  # strip times
+        self.assertEqual(out, ref_out)
+        self.assertEqual(err, [])
+        # Get info
+        group_nums = gmsh.model.getPhysicalGroups()
+        names = [gmsh.model.getPhysicalName(*grp) for grp in group_nums]
+        ref_names = list(ref_groups.keys())
+        # Check correct group names/entities
+        for i, name in enumerate(names):
+            self.assertEqual(name, ref_names[i])
+            index = names.index(name)
+            group_ents = list(gmsh.model.getEntitiesForPhysicalGroup(*group_nums[index]))
+            ref_group_ents = ref_groups[name]
+            self.assertEqual(group_ents, ref_group_ents)
+        # Check correct area/centroid
+        for ent in gmsh.model.getEntities(2):
+            tag = ent[1]
+            mass = gmsh.model.occ.getMass(2, tag)
+            self.assertAlmostEqual(ref_areas[tag], mass, places=5)
+            x, y, z = gmsh.model.occ.getCenterOfMass(2, tag)
+            centroid = (x, y, z)
+            for i in range(3):
+                self.assertAlmostEqual(centroid[i], ref_centroids[tag][i])
+        # Clean up
+        gmsh.clear()
+        gmsh.finalize()
+
+    def test_2_rectangles_with_bad_overwrite(self):
+        """Test a 2 rectangle case, overwriting a material that doesnt exist."""
+        with pytest.raises(SystemExit):
+            with captured_output() as (out, err):
+                mocmg.initialize()
+                gmsh.initialize()
+                gmsh.model.occ.addRectangle(0.0, 0.0, 0.0, 2.0, 2.0)
+                gmsh.model.occ.addRectangle(1.0, 1.0, 0.0, 2.0, 2.0)
+                gmsh.model.occ.synchronize()
+                gmsh.model.addPhysicalGroup(2, [1])
+                gmsh.model.addPhysicalGroup(2, [2])
+                gmsh.model.addPhysicalGroup(2, [1, 2])
+                gmsh.model.setPhysicalName(2, 1, "Material_1")
+                gmsh.model.setPhysicalName(2, 2, "Material_2")
+                gmsh.model.setPhysicalName(2, 3, "All")
+                mocmg.model.group_preserving_fragment(
+                    [(2, 1)], [(2, 2)], overwrite_material="BAD_MAT"
+                )
+        gmsh.clear()
+        gmsh.finalize()
+        out, err = out.getvalue().splitlines(), err.getvalue().splitlines()
+        out = [line.split(None, 1)[1] for line in out]
+        err = [line.split(None, 1)[1] for line in [err[0]]]  # strip times
+        self.assertEqual(out, ref_out)
+        self.assertEqual(err, bad_overwrite)
